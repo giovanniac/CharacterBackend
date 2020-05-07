e5cc775adfe08df31b4def5ff46a9bec9247cde9
Giovanni Caracho <giovanni.andreassa@paxtecnologia.com.br>
2020-05-07 16:14:22 -0300

    Runner

--- src/main/java/br/com/paxtecnologia/backend/runner/Application.java
+++ src/main/java/br/com/paxtecnologia/backend/runner/Application.java
@@ -1,4 +1,4 @@
-package br.com.paxtecnologia.backend.estagio;
+package br.com.paxtecnologia.backend.runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
