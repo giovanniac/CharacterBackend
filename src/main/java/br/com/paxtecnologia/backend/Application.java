d5bc0ccef5966800c4544a4e0a83ae42fdb3f6aa
Giovanni Caracho <giovanni.caracho@yahoo.com.br>
2020-05-08 11:23:13 -0300

    update application.java

--- src/main/java/br/com/paxtecnologia/backend/Application.java
+++ src/main/java/br/com/paxtecnologia/backend/Application.java
@@ -6,18 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-//	private CharacterRepository characterRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//		characterRepository.save(new Character(1,"Robert"));
-//		characterRepository.save(new Character(2,"Iron Man"));	
-//		characterRepository.save(new Character(3,"Shiryu"));	
-//	}
-//	
 
 }
